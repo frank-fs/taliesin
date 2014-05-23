840f6043333dc29918f950ca01225335bb97c7e0
Ryan Riley <ryan@tachyus.com>
2014-05-23 11:08:24 -0500

    Update project structure.

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,2 @@
+### New in 0.1.0 - (TBD)
+* First official release.
---
+++ build.fsx
@@ -0,0 +1,203 @@
+// --------------------------------------------------------------------------------------
+// FAKE build script 
+// --------------------------------------------------------------------------------------
+
+#r "packages/FAKE/tools/FakeLib.dll"
+#r "packages/FAKE/tools/NuGet.Core.dll"
+#load "packages/SourceLink.Fake/tools/SourceLink.Tfs.fsx"
+open System
+open System.IO
+open Fake 
+open Fake.AssemblyInfoFile
+open Fake.Git
+open Fake.ReleaseNotesHelper
+open SourceLink
+
+// --------------------------------------------------------------------------------------
+// Provide project-specific details below
+// --------------------------------------------------------------------------------------
+
+// Information about the project are used
+//  - for version and project name in generated AssemblyInfo file
+//  - by the generated NuGet package 
+//  - to run tests and to publish documentation on GitHub gh-pages
+//  - for documentation, you also need to edit info in "docs/tools/generate.fsx"
+
+// The name of the project 
+// (used by attributes in AssemblyInfo, name of a NuGet package and directory in 'src')
+let project = "Taliesin"
+
+// Short summary of the project
+// (used as description in AssemblyInfo and as a short summary for NuGet package)
+let summary = "Web application routing library using F# Agents"
+
+// Longer description of the project
+// (used as a description for NuGet package; line breaks are automatically cleaned up)
+let description = """
+  Web application routing library using F# Agents"""
+// List of author names (for NuGet package)
+let authors = [ "Ryan Riley" ]
+// Tags for your project (for NuGet package)
+let tags = "F# fsharp web http router routing"
+
+// File system information 
+// (<projectFile>.*proj is built during the building process)
+let projectFile = "Taliesin"
+// Pattern specifying assemblies to be tested using NUnit
+let testAssemblies = "bin/Taliesin*Tests*exe"
+
+// Git configuration (used for publishing documentation in gh-pages branch)
+// The profile where the project is posted 
+let gitHome = "git@github.com:panesofglass"
+// The name of the project on GitHub
+let gitName = "taliesin"
+
+// --------------------------------------------------------------------------------------
+// The rest of the file includes standard build steps 
+// --------------------------------------------------------------------------------------
+
+// Read additional information from the release notes document
+Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+let release = parseReleaseNotes (IO.File.ReadAllLines "RELEASE_NOTES.md")
+
+// Generate assembly info files with the right version & up-to-date information
+Target "AssemblyInfo" (fun _ ->
+  let fileName = "src/" + project + "/AssemblyInfo.fs"
+  CreateFSharpAssemblyInfo fileName
+      [ Attribute.Title project
+        Attribute.Product project
+        Attribute.Description summary
+        Attribute.Version release.AssemblyVersion
+        Attribute.FileVersion release.AssemblyVersion ] )
+
+// --------------------------------------------------------------------------------------
+// Clean build results & restore NuGet packages
+
+Target "RestorePackages" RestorePackages
+
+Target "Clean" (fun _ ->
+    CleanDirs ["bin"; "temp"]
+)
+
+Target "CleanDocs" (fun _ ->
+    CleanDirs ["docs/output"]
+)
+
+// --------------------------------------------------------------------------------------
+// Build library & test project
+
+Target "BuildNumber" (fun _ ->
+    use tb = getTfsBuild()
+    tb.Build.BuildNumber <- sprintf "Frank.%s.%s" release.AssemblyVersion tb.Build.BuildNumber
+    tb.Build.Save()
+)
+
+Target "Build" (fun _ ->
+    !! ("*/**/" + projectFile + "*.*proj")
+    |> MSBuildRelease "bin" "Rebuild"
+    |> ignore
+)
+
+Target "SourceLink" (fun _ ->
+    #if MONO
+    ()
+    #else
+    use repo = new GitRepo(__SOURCE_DIRECTORY__)
+    !! ("*/**/" + projectFile + "*.*proj")
+    |> Seq.iter (fun f ->
+        let proj = VsProj.Load f ["Configuration", "Release"; "OutputPath", Path.combine __SOURCE_DIRECTORY__ "bin"]
+        logfn "source linking %s" proj.OutputFilePdb
+        let files = proj.Compiles -- "**/AssemblyInfo.fs"
+        repo.VerifyChecksums files
+        proj.VerifyPdbChecksums files
+        proj.CreateSrcSrv "https://raw.github.com/panesofglass/taliesin/{0}/%var2%" repo.Revision (repo.Paths files)
+        Pdbstr.exec proj.OutputFilePdb proj.OutputFilePdbSrcSrv
+    )
+    #endif
+)
+
+Target "CopyLicense" (fun _ ->
+    [ "LICENSE.txt" ] |> CopyTo "bin"
+)
+
+// --------------------------------------------------------------------------------------
+// Run the unit tests using test runner
+
+Target "RunTests" (fun _ ->
+    let errorCode =
+        [ for program in !!testAssemblies do
+            let p, a = if not isMono then program, null else "mono", program
+            let result = asyncShellExec { defaultParams with Program = p; CommandLine = a } |> Async.RunSynchronously
+            yield result ]
+        |> List.sum
+    if errorCode <> 0 then failwith "Error in tests"
+)
+
+// --------------------------------------------------------------------------------------
+// Build a NuGet package
+
+Target "NuGet" (fun _ ->
+    let bin = if isTfsBuild then "../bin" else "bin"
+    Directory.CreateDirectory bin |> ignore
+    NuGet (fun p -> 
+        { p with   
+            Authors = authors
+            Project = project
+            Summary = summary
+            Description = description
+            Version = release.NugetVersion
+            ReleaseNotes = String.Join(Environment.NewLine, release.Notes)
+            Tags = tags
+            OutputPath = bin
+            AccessKey = getBuildParamOrDefault "nugetkey" ""
+            Publish = hasBuildParam "nugetkey" })
+        ("nuget/" + project + ".nuspec")
+)
+
+// --------------------------------------------------------------------------------------
+// Generate the documentation
+
+Target "GenerateDocs" (fun _ ->
+    executeFSIWithArgs "docs/tools" "generate.fsx" ["--define:RELEASE"] [] |> ignore
+)
+
+// --------------------------------------------------------------------------------------
+// Release Scripts
+
+Target "ReleaseDocs" (fun _ ->
+    let tempDocsDir = "temp/gh-pages"
+    CleanDir tempDocsDir
+    Repository.cloneSingleBranch "" (gitHome + "/" + gitName + ".git") "gh-pages" tempDocsDir
+
+    fullclean tempDocsDir
+    CopyRecursive "docs/output" tempDocsDir true |> tracefn "%A"
+    StageAll tempDocsDir
+    Commit tempDocsDir (sprintf "Update generated documentation for version %s" release.NugetVersion)
+    Branches.push tempDocsDir
+)
+
+Target "Release" DoNothing
+
+// --------------------------------------------------------------------------------------
+// Run all targets by default. Invoke 'build <Target>' to override
+
+Target "All" DoNothing
+
+"Clean"
+  ==> "RestorePackages"
+  =?> ("BuildNumber", isTfsBuild)
+  ==> "AssemblyInfo"
+  ==> "Build"
+  =?> ("SourceLink", isMono = false && hasBuildParam "skipSourceLink" = false)
+  ==> "CopyLicense"
+  ==> "RunTests"
+  ==> "NuGet"
+  ==> "All"
+
+"All" 
+  ==> "CleanDocs"
+  ==> "GenerateDocs"
+  ==> "ReleaseDocs"
+  ==> "Release"
+
+RunTargetOrDefault "All"
